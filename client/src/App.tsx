026428d8b3bed7607dec1f4e4a212ac2692dcef4
shugulo977 <51476137-shugulo977@users.noreply.replit.com>
2026-01-06 13:44:01 +0000

    Restored to '7f697b40b1fc0a3c3d8556ec213cc79ceb05e6b3'
    Replit-Restored-To: 7f697b40b1fc0a3c3d8556ec213cc79ceb05e6b3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,6 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ScrollProgressBar } from "@/components/ScrollProgressBar";
-import { BackToTopButton } from "@/components/BackToTopButton";
 import Home from "@/pages/home";
 import Register from "@/pages/register";
 import AboutUs from "@/pages/about-us";
@@ -30,7 +29,6 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <ScrollProgressBar />
-        <BackToTopButton />
         <Toaster />
         <Router />
       </TooltipProvider>
